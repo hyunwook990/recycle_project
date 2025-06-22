e62343756ec09925bc57e4c5a6057e8f5a7167ab
hyunwook990 <jho99061288@gmail.com>
2025-06-22 15:23:28 +0900

    pyqt5 구성 변경

--- main_page.py
+++ main_page.py
@@ -4,13 +4,11 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.recycle()
-    
-    def recycle(self):
-
-
+        
         self.setWindowTitle("recycle_project")
-        self.setGeometry(300, 300, 300, 300)
+        # 모니터를 x, y축으로 구분, (모니터의 왼쪽 상단은 (0, 0))
+        # x, y, 창의 넓이, 창의 높이 (좌표의 기준은 창의 왼쪽 상단)
+        self.setGeometry(300, 100, 1500, 900)
         self.show()
 
 if __name__ == "__main__":
