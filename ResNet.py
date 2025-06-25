d5252dbac532fe2667eac745229fd16d48f143ba
hyunwook990 <jho99061288@gmail.com>
2025-06-25 23:01:22 +0900

    downsampling 층 분리

--- ResNet.py
+++ ResNet.py
@@ -31,7 +31,7 @@ class ResNet50(nn.Module):
             nn.BatchNorm2d(),
             nn.ReLU()
         )
-        self.conv3_x = nn.Sequential(
+        self.conv3_x_down = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=128, kernel_size=1, stride=2),
             nn.BatchNorm2d(),
             nn.ReLU(),
@@ -42,7 +42,18 @@ class ResNet50(nn.Module):
             nn.BatchNorm2d(),
             nn.ReLU()
         )
-        self.conv4_x = nn.Sequential(
+        self.conv3_x = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=128, kernel_size=1, stride=1),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=512, kernel_size=1, stride=1),
+            nn.BatchNorm2d(),
+            nn.ReLU()
+        )
+        self.conv4_x_down = nn.Sequential(
             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=2),
             nn.BatchNorm2d(),
             nn.ReLU(),
@@ -53,7 +64,18 @@ class ResNet50(nn.Module):
             nn.BatchNorm2d(),
             nn.ReLU()
         )
-        self.conv5_x = nn.Sequential(
+        self.conv4_x = nn.Sequential(
+            nn.Conv2d(in_channels=1024, out_channels=256, kernel_size=1, stride=1),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=1024, kernel_size=1, stride=1),
+            nn.BatchNorm2d(),
+            nn.ReLU()
+        )
+        self.conv5_x_down = nn.Sequential(
             nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1, stride=2),
             nn.BatchNorm2d(),
             nn.ReLU(),
@@ -62,14 +84,37 @@ class ResNet50(nn.Module):
             nn.ReLU(),
             nn.Conv2d(in_channels=512, out_channels=2048, kernel_size=1, stride=1),
             nn.BatchNorm2d(),
+            nn.ReLU()
+        )
+        self.conv5_x = nn.Sequential(
+            nn.Conv2d(in_channels=2048, out_channels=512, kernel_size=1, stride=1),
+            nn.BatchNorm2d(),
             nn.ReLU(),
-            nn.AvgPool2d(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=2048, kernel_size=1, stride=1),
+            nn.BatchNorm2d(),
+            nn.ReLU()
+        )
+        self.linear = nn.Sequential(
             nn.Flatten(),
+            nn.AvgPool2d(),
             nn.Linear(in_features=7*7*2048, out_features=1000)
         )
+
+
     def forward(self, x):
         x = self.conv1(x)
         for _ in range(3):
             x = self.conv2_x(x)
-        for _ in range(4):
-            x = self.conv3_x()
+        x = self.conv3_x_down(x)
+        for _ in range(3):
+            x = self.conv3_x(x)
+        x = self.conv4_x_down(x)
+        for _ in range(5):
+            x = self.conv4_x(x)
+        x = self.conv5_x_down(x)
+        for _ in range(2):
+            x = self.conv5_x(x)
+        return self.linear(x)
