91d5b17aca7ce5d9ec7270331653f39a5e62b854
hyunwook990 <jho99061288@gmail.com>
2025-06-26 23:43:04 +0900

    residual block(shortcut) 구현 1차완료

--- ResNet.py
+++ ResNet.py
@@ -21,8 +21,19 @@ class ResNet50(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
             # 56
         )
-        self.conv2_x = nn.Sequential(
+        self.conv2_x_1 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1),
+            nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=1, stride=1),
+            nn.BatchNorm2d(num_features=256, eps=1e-4, momentum=.9),
+            nn.ReLU()
+        )
+        self.conv2_x = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=64, kernel_size=1, stride=1),
             nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
             nn.ReLU(),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
@@ -106,22 +117,67 @@ class ResNet50(nn.Module):
             nn.Flatten(),
             nn.Linear(in_features=7*7*2048, out_features=1000)
         )
+        self.projection_shortcut_2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=1, stride=1),
+            nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
+            nn.ReLU()
+        )
+        self.projection_shortcut_3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=1),
+            nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
+            nn.ReLU()
+        )
+        self.projection_shortcut_4 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=1, stride=1),
+            nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
+            nn.ReLU()
+        )
+        self.projection_shortcut_5 = nn.Sequential(
+            nn.Conv2d(in_channels=1024, out_channels=2048, kernel_size=1, stride=1),
+            nn.BatchNorm2d(num_features=64, eps=1e-4, momentum=.9),
+            nn.ReLU()
+        )
 
 
     def forward(self, x):
         x = self.conv1(x)           # 64
-        shortcut = x
-        for _ in range(3):
-            x = self.conv2_x(x)     # 256
-        x = self.conv3_x_down(x)
-        for _ in range(3):
-            x = self.conv3_x(x)     # 512
-        x = self.conv4_x_down(x)
-        for _ in range(5):
-            x = self.conv4_x(x)     # 1024
-        x = self.conv5_x_down(x)
+        temp = x                    # 64
+        
+        x = self.conv2_x_1(x)       # 64 -> 256
+        shortcut = self.projection_shortcut_2(temp) # 64 -> 256
+        temp = x                    # 256
+        
         for _ in range(2):
-            x = self.conv5_x(x)     # 2048
+            x = self.conv2_x(x+shortcut) # 256 -> 256
+            shortcut = temp         # 256
+            temp = x                # 256
+        
+        x = self.conv3_x_down(x+shortcut) # 256 -> 512
+        shortcut = self.projection_shortcut_3(temp) # 256 -> 512
+        temp = x                    # 512
+        
+        for _ in range(3):
+            x = self.conv3_x(x+shortcut) # 512 -> 512
+            shortcut = temp         # 512
+            temp = x                # 512
+        
+        x = self.conv4_x_down(x+shortcut)    # 512 -> 1024
+        shortcut = self.projection_shortcut_4(temp) # 512 -> 1024
+        temp = x                    # 1024
+        for _ in range(5):
+            x = self.conv4_x(x+shortcut) # 1024 -> 1024
+            shortcut = temp         # 1024
+            temp = x                # 1024
+        
+        x = self.conv5_x_down(x+shortcut)    # 1024 -> 2048
+        shortcut = self.projection_shortcut_5(temp) # 1024 -> 2048
+        temp = x                    # 2048
+        
+        for _ in range(2):
+            x = self.conv5_x(x+shortcut) # 2048 -> 2048
+            shortcut = temp         # 2048
+            temp = x                # 2048
+
         return self.linear(x)
     
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
